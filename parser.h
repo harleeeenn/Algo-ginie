20f6cbad69a8841fe4d6c48ec54ecf67825fa4b9
NeetuChaudhary1 <148558982+NeetuChaudhary1@users.noreply.github.com>
2025-06-24 00:28:22 +0530

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,5 +1,14 @@
 #ifndef PARSER_H
 #define PARSER_H
+
 #include "lexer.h"
-void parse(Token* tokens);
-#endif
+
+void parse(Token* tokens);  
+
+void generateHTMLTree(const char* htmlFilePath);
+void finalizeHTMLTree();
+void printTreeNode(const char* label, const char* id);
+void closeTreeNode();
+
+
+#endif  
