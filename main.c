60f0b1164d8cd6aa2c5b350128469fbad52a126e
NeetuChaudhary1 <148558982+NeetuChaudhary1@users.noreply.github.com>
2025-06-24 00:30:31 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,23 @@
 #include "lexer.h"
 #include "parser.h"
 
+extern char parseTreeOutput[10000];  
+
 int main() {
     int tokenCount = 0;
-    Token* tokens = tokenize("input.c", &tokenCount);  
+    Token* tokens = tokenize("input.c", &tokenCount);
 
-    for (int i = 0; i < tokenCount; i++) {
-    printf("Token %d: Type = %d, Lexeme = %s\n", i, tokens[i].type, tokens[i].lexeme);
-}
-
+    
     printf("Pseudocode:\n");
-    parse(tokens);  
-    free(tokens); 
+    parse(tokens); 
+    printf("===END===\n");
+    
+    printf("Tokens:\n");
+    for (int i = 0; i < tokenCount; i++) {
+        printf("Token %d: Type = %d, Lexeme = %s\n", i, tokens[i].type, tokens[i].lexeme);
+    }
+    printf("===END===\n");
+
+    free(tokens);
     return 0;
 }
