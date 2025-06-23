56c7805d3c38baa95a0f349a89131c65f6f48fea
NeetuChaudhary1 <148558982+NeetuChaudhary1@users.noreply.github.com>
2025-06-24 01:42:32 +0530

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,58 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdarg.h>  
-
+#include <stdarg.h>
 #include "parser.h"
+#include "tree.h"
 
-void printTreeNode(const char* label, const char* id);
-void closeTreeNode();
-
-
-FILE* htmlTreeFile;
 int nodeCount = 0;
-
-void startHTMLTree() {
-    htmlTreeFile = fopen("tree.html", "w");
-    if (!htmlTreeFile) return;
-
-    fprintf(htmlTreeFile,
-        "<!DOCTYPE html><html><head><meta charset='UTF-8'><style>"
-        ".tree ul { padding-top: 20px; position: relative; }"
-        ".tree li { float: left; text-align: center; list-style-type: none; position: relative; padding: 20px 5px 0 5px; }"
-        ".tree li::before, .tree li::after { content: ''; position: absolute; top: 0; right: 50%%; border-top: 2px solid #ccc; width: 50%%; height: 20px; }"
-        ".tree li::after { right: auto; left: 50%%; border-left: 2px solid #ccc; }"
-        ".tree li:only-child::before, .tree li:only-child::after { display: none; }"
-        ".tree li:only-child { padding-top: 0; }"
-        ".tree li:first-child::before, .tree li:last-child::after { border: 0 none; }"
-        ".tree li:last-child::before { border-right: 2px solid #ccc; border-radius: 0 5px 0 0; }"
-        ".tree li:first-child::after { border-radius: 5px 0 0 0; }"
-        ".tree .node { display: inline-block; border: 2px solid #4CAF50; padding: 10px 20px; border-radius: 20px; background: #eaffea; font-weight: bold; }"
-        "</style></head><body><div class='tree'><ul>\n"
-    );
-}
-
-void endHTMLTree() {
-    fprintf(htmlTreeFile, "</ul></div></body></html>");
-    fclose(htmlTreeFile);
-}
-
-void openNode(const char* label) {
-    fprintf(htmlTreeFile, "<li><div class='node'>%s</div><ul>\n", label);
-}
-
-void closeNode() {
-    fprintf(htmlTreeFile, "</ul></li>\n");
-}
-
-void leafNode(const char* label) {
-    fprintf(htmlTreeFile, "<li><div class='node'>%s</div></li>\n", label);
-}
 
 void printPseudo(const char* format, ...) {
     va_list args;
     va_start(args, format);
-    vprintf(format, args);  
+    vprintf(format, args);
     va_end(args);
 }
 
@@ -65,29 +23,47 @@ void parse(Token* tokens) {
 
     while (tokens[i].type != TOKEN_EOF) {
         // Variable declaration
-        if (tokens[i].type == TOKEN_INT) {
-            i++;
-            while (tokens[i].type == TOKEN_IDENTIFIER) {
-                openNode("Declaration (int)");
-                leafNode(tokens[i].lexeme);
-                printPseudo("Declare int %s\n", tokens[i].lexeme);
-                i++;
+   if (tokens[i].type == TOKEN_INT) {
+    i++;  // skip 'int'
 
-                if (tokens[i].type == TOKEN_ASSIGN) {
+    while (1) {
+        if (tokens[i].type == TOKEN_IDENTIFIER) {
+            openNode("Declaration (int)");
+            char varName[100];
+            strcpy(varName, tokens[i].lexeme);
+            leafNode(varName);
+            printPseudo("Declare int %s\n", varName);
+            i++;
+
+            if (tokens[i].type == TOKEN_ASSIGN) {
+                i++;
+                if (tokens[i].type == TOKEN_NUMBER || tokens[i].type == TOKEN_IDENTIFIER) {
+                    openNode("Assignment");
+                    leafNode(varName);
+                    leafNode(tokens[i].lexeme);
+                    printPseudo("Assign %s = %s\n", varName, tokens[i].lexeme);
                     i++;
-                    if (tokens[i].type == TOKEN_NUMBER || tokens[i].type == TOKEN_IDENTIFIER) {
-                        openNode("Assignment");
-                        leafNode(tokens[i - 2].lexeme); // variable name
-                        leafNode(tokens[i].lexeme);    // value
-                        printPseudo("Assign %s = %s\n", tokens[i - 2].lexeme, tokens[i].lexeme);
-                        closeNode(); // Assignment
-                        i++;
-                    }
+                    closeNode(); // Assignment
                 }
-                if (tokens[i].type == TOKEN_COMMA || tokens[i].type == TOKEN_SEMICOLON) i++;
-                closeNode(); // Declaration
             }
+
+            closeNode(); // Declaration
+        } else {
+            break;
         }
+
+        if (tokens[i].type == TOKEN_COMMA) {
+            i++;
+        } else if (tokens[i].type == TOKEN_SEMICOLON) {
+            i++;
+            break;
+        } else {
+            break;  // to be safe
+        }
+    }
+}
+
+
 
         // Assignment
         else if (tokens[i].type == TOKEN_IDENTIFIER) {
@@ -128,18 +104,107 @@ void parse(Token* tokens) {
             }
         }
 
-       
-
-
-        // For loop
-        else if (tokens[i].type == TOKEN_FOR) {
-            openNode("For Loop");
-            printPseudo("For Loop Detected:\n");
-            i++; // for
+        // If Statement
+        else if (tokens[i].type == TOKEN_IF) {
+            openNode("If Statement");
+            i++; // skip 'if'
 
             if (tokens[i].type == TOKEN_ROUND_BRACE_OPEN) i++;
 
-            // Init
+            char left[50], op[5], right[50];
+            strcpy(left, tokens[i++].lexeme);
+            strcpy(op, tokens[i++].lexeme);
+            strcpy(right, tokens[i++].lexeme);
+
+            leafNode("Condition:");
+            leafNode(left);
+            leafNode(op);
+            leafNode(right);
+            printPseudo("If (%s %s %s)\n", left, op, right);
+
+            if (tokens[i].type == TOKEN_ROUND_BRACE_CLOSE) i++;
+
+            if (tokens[i].type == TOKEN_OPEN_BRACE) {
+                i++; openNode("Body"); printPseudo("Begin Block\n");
+
+                while (tokens[i].type != TOKEN_CLOSE_BRACE && tokens[i].type != TOKEN_EOF) {
+                    if (tokens[i].type == TOKEN_IDENTIFIER) {
+                        char varName[50]; strcpy(varName, tokens[i++].lexeme);
+
+                        if (tokens[i].type == TOKEN_ASSIGN) {
+                            i++;
+                            char left[50]; strcpy(left, tokens[i++].lexeme);
+
+                            if (tokens[i].type == TOKEN_OPERATOR) {
+                                char op = tokens[i++].lexeme[0];
+                                char right[50]; strcpy(right, tokens[i++].lexeme);
+
+                                openNode("Assignment");
+                                leafNode(varName);
+                                char expr[100]; sprintf(expr, "%s %c %s", left, op, right);
+                                leafNode(expr);
+                                closeNode();
+                                printPseudo("Set %s = %s\n", varName, expr);
+                            }
+                        }
+                        if (tokens[i].type == TOKEN_SEMICOLON) i++;
+                    }
+                    else if (tokens[i].type == TOKEN_PRINT) {
+                        i++;
+                        while (tokens[i].type != TOKEN_STRING_LITERAL && tokens[i].type != TOKEN_EOF) i++;
+                        if (tokens[i].type == TOKEN_STRING_LITERAL) i++;
+
+                        while (tokens[i].type == TOKEN_COMMA) {
+                            i++;
+                            if (tokens[i].type == TOKEN_IDENTIFIER) {
+                                leafNode(tokens[i].lexeme);
+                                printPseudo("Print %s", tokens[i].lexeme);
+                                i++;
+                            }
+                        }
+                        if (tokens[i].type == TOKEN_SEMICOLON) i++;
+                    }
+                    else i++;
+                }
+
+                closeNode(); // Body
+                printPseudo("End Block\n");
+                i++;
+            }
+            closeNode(); // If
+        }
+
+        // Print Statement
+        else if (tokens[i].type == TOKEN_PRINT) {
+            i++; openNode("Print Statement");
+
+            if (tokens[i].type == TOKEN_STRING_LITERAL) {
+                i++;
+                while (tokens[i].type != TOKEN_STRING_LITERAL && tokens[i].type != TOKEN_EOF) i++;
+                if (tokens[i].type == TOKEN_STRING_LITERAL) i++;
+            }
+
+            while (tokens[i].type == TOKEN_COMMA) {
+                i++;
+                if (tokens[i].type == TOKEN_IDENTIFIER) {
+                    leafNode(tokens[i].lexeme);
+                    printPseudo("Print %s\n", tokens[i].lexeme);
+                    i++;
+                }
+            }
+
+            if (tokens[i].type == TOKEN_SEMICOLON) i++;
+            closeNode();
+        }
+
+        // For Loop
+        else if (tokens[i].type == TOKEN_FOR) {
+            openNode("For Loop");
+            printPseudo("For Loop Detected:\n");
+            i++;
+
+            if (tokens[i].type == TOKEN_ROUND_BRACE_OPEN) i++;
+
             openNode("Init");
             printPseudo("  Init: ");
             while (tokens[i].type != TOKEN_SEMICOLON) {
@@ -147,10 +212,8 @@ void parse(Token* tokens) {
                 leafNode(tokens[i].lexeme);
                 i++;
             }
-            printPseudo("\n");
             i++; closeNode();
 
-            // Condition
             openNode("Condition");
             printPseudo("  Condition: ");
             while (tokens[i].type != TOKEN_SEMICOLON) {
@@ -158,10 +221,8 @@ void parse(Token* tokens) {
                 leafNode(tokens[i].lexeme);
                 i++;
             }
-            printPseudo("\n");
             i++; closeNode();
 
-            // Increment
             openNode("Increment");
             printPseudo("  Increment: ");
             while (tokens[i].type != TOKEN_ROUND_BRACE_CLOSE) {
@@ -169,22 +230,18 @@ void parse(Token* tokens) {
                 leafNode(tokens[i].lexeme);
                 i++;
             }
-            printPseudo("\n");
             i++; closeNode();
 
-            // Body
             if (tokens[i].type == TOKEN_OPEN_BRACE) {
-                i++; openNode("Body");
-                printPseudo("  Body:\n");
+                i++; openNode("Body"); printPseudo("  Body:\n");
                 while (tokens[i].type != TOKEN_CLOSE_BRACE && tokens[i].type != TOKEN_EOF) {
                     printPseudo("  %s", tokens[i].lexeme);
                     leafNode(tokens[i].lexeme);
                     i++;
                 }
-                i++; closeNode(); // Body
+                i++; closeNode();
             }
-
-            closeNode(); // For Loop
+            closeNode();
         }
 
         else {
