c7b8a8a7f62e1ea4cb0ee12f06458fe432e8fd77
NeetuChaudhary1 <148558982+NeetuChaudhary1@users.noreply.github.com>
2025-06-24 00:31:29 +0530

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -4,125 +4,136 @@
 #include <ctype.h>
 #include "lexer.h"
 
+#define MAX_TOKENS 1000
+
+int isKeyword(const char* word) {
+    return strcmp(word, "int") == 0 ||
+           strcmp(word, "for") == 0 ||
+           strcmp(word, "while") == 0 ||
+           strcmp(word, "if") == 0 ||
+           strcmp(word, "else") == 0 ||
+           strcmp(word, "return") == 0 ||
+           strcmp(word, "print") == 0 ||
+           strcmp(word, "printf") == 0;
+}
+
+TokenType getKeywordType(const char* word) {
+    if (strcmp(word, "int") == 0) return TOKEN_INT;
+    if (strcmp(word, "for") == 0) return TOKEN_FOR;
+    if (strcmp(word, "while") == 0) return TOKEN_WHILE;
+    if (strcmp(word, "if") == 0) return TOKEN_IF;
+    if (strcmp(word, "else") == 0) return TOKEN_ELSE;
+    if (strcmp(word, "return") == 0) return TOKEN_RETURN;
+    if (strcmp(word, "print") == 0 || strcmp(word, "printf") == 0) return TOKEN_PRINT;
+    return TOKEN_IDENTIFIER;
+}
+
 Token* tokenize(const char* filename, int* tokenCount) {
     FILE* file = fopen(filename, "r");
     if (!file) {
-        printf("Error opening file!\n");
+        perror("Error opening file");
         exit(1);
     }
 
     Token* tokens = malloc(sizeof(Token) * MAX_TOKENS);
     *tokenCount = 0;
+
     char ch;
     while ((ch = fgetc(file)) != EOF) {
-        if (isspace(ch)) {
-            continue;
-        } else if (ch == '#') {
-            while ((ch = fgetc(file)) != '\n' && ch != EOF);
-            continue;
-        } else if (isalpha(ch) || ch == '_') {
+        if (isspace(ch)) continue;
+
+        // Identifiers / keywords
+        if (isalpha(ch)) {
             char buffer[100] = {0};
-            int i = 0;
-            buffer[i++] = ch;
-            while (isalnum(ch = fgetc(file)) || ch == '_') {
-                buffer[i++] = ch;
+            int len = 0;
+            buffer[len++] = ch;
+            while (isalnum(ch = fgetc(file))) {
+                buffer[len++] = ch;
             }
-            buffer[i] = '\0';
+            buffer[len] = '\0';
             ungetc(ch, file);
 
-            if (strcmp(buffer, "int") == 0) tokens[*tokenCount].type = TOKEN_INT;
-            else if (strcmp(buffer, "if") == 0) tokens[*tokenCount].type = TOKEN_IF;
-            else if (strcmp(buffer, "else") == 0) tokens[*tokenCount].type = TOKEN_ELSE;
-            else if (strcmp(buffer, "for") == 0) tokens[*tokenCount].type = TOKEN_FOR;
-            else if (strcmp(buffer, "while") == 0) tokens[*tokenCount].type = TOKEN_WHILE;
-            else if (strcmp(buffer, "return") == 0) tokens[*tokenCount].type = TOKEN_RETURN;
-            else if (strcmp(buffer, "printf") == 0) tokens[*tokenCount].type = TOKEN_PRINT;
-            else tokens[*tokenCount].type = TOKEN_IDENTIFIER;
+            Token t;
+            if (isKeyword(buffer)) {
+                t.type = getKeywordType(buffer);
+            } else {
+                t.type = TOKEN_IDENTIFIER;
+            }
+            strcpy(t.lexeme, buffer);
+            tokens[(*tokenCount)++] = t;
+        }
 
-            strcpy(tokens[*tokenCount].lexeme, buffer);
-            (*tokenCount)++;
-        } else if (isdigit(ch)) {
+        // Numbers
+        else if (isdigit(ch)) {
             char buffer[100] = {0};
-            int i = 0;
-            buffer[i++] = ch;
+            int len = 0;
+            buffer[len++] = ch;
             while (isdigit(ch = fgetc(file))) {
-                buffer[i++] = ch;
+                buffer[len++] = ch;
             }
-            buffer[i] = '\0';
+            buffer[len] = '\0';
             ungetc(ch, file);
 
-            tokens[*tokenCount].type = TOKEN_NUMBER;
-            strcpy(tokens[*tokenCount].lexeme, buffer);
-            (*tokenCount)++;
-        } else {
-            switch (ch) {
-                case '{':
-                    tokens[*tokenCount].type = TOKEN_OPEN_BRACE;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
-                case '}':
-                    tokens[*tokenCount].type = TOKEN_CLOSE_BRACE;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
-                case '(':
-                    tokens[*tokenCount].type = TOKEN_ROUND_BRACE_OPEN;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break; 
-                case ')':
-                    tokens[*tokenCount].type = TOKEN_ROUND_BRACE_CLOSE;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
-                case '"':
-                    tokens[*tokenCount].type = TOKEN_SLIT;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;    
-                case ',':
-                    tokens[*tokenCount].type = TOKEN_COMMA;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;           
-                case ';':
-                    tokens[*tokenCount].type = TOKEN_SEMICOLON;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
-                case '=':
-                    tokens[*tokenCount].type = TOKEN_ASSIGN;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
-                case '+': case '-': case '*': case '/':
-                    tokens[*tokenCount].type = TOKEN_OPERATOR;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
-                default:
-                    tokens[*tokenCount].type = TOKEN_UNKNOWN;
-                    tokens[*tokenCount].lexeme[0] = ch;
-                    tokens[*tokenCount].lexeme[1] = '\0';
-                    (*tokenCount)++;
-                    break;
+            Token t = {TOKEN_NUMBER};
+            strcpy(t.lexeme, buffer);
+            tokens[(*tokenCount)++] = t;
+        }
+
+        // String literal
+        else if (ch == '"') {
+            char buffer[100] = {0};
+            int len = 0;
+            while ((ch = fgetc(file)) != '"' && ch != EOF) {
+                buffer[len++] = ch;
             }
+            buffer[len] = '\0';
+
+            Token t = {TOKEN_STRING_LITERAL};
+            strcpy(t.lexeme, buffer);
+            tokens[(*tokenCount)++] = t;
+        }
+
+        // Two-character operators
+        else if (ch == '=' || ch == '!' || ch == '<' || ch == '>') {
+            char next = fgetc(file);
+            if (next == '=') {
+                Token t = {TOKEN_OPERATOR};
+                sprintf(t.lexeme, "%c=", ch);
+                tokens[(*tokenCount)++] = t;
+            } else {
+                ungetc(next, file);
+                Token t = {TOKEN_OPERATOR};
+                sprintf(t.lexeme, "%c", ch);
+                tokens[(*tokenCount)++] = t;
+            }
+        }
+
+        // Single-character operators and punctuation
+        else {
+            Token t;
+            switch (ch) {
+                case '+': case '-': case '*': case '/': case '%':
+                    t.type = TOKEN_OPERATOR;
+                    break;
+                case '=': t.type = TOKEN_ASSIGN; break;
+                case ';': t.type = TOKEN_SEMICOLON; break;
+                case ',': t.type = TOKEN_COMMA; break;
+                case '{': t.type = TOKEN_OPEN_BRACE; break;
+                case '}': t.type = TOKEN_CLOSE_BRACE; break;
+                case '(': t.type = TOKEN_ROUND_BRACE_OPEN; break;
+                case ')': t.type = TOKEN_ROUND_BRACE_CLOSE; break;
+                default:
+                    printf("Unknown character: %c\n", ch);
+                    continue;
+            }
+            t.lexeme[0] = ch;
+            t.lexeme[1] = '\0';
+            tokens[(*tokenCount)++] = t;
         }
     }
 
-    tokens[*tokenCount].type = TOKEN_EOF;
-    strcpy(tokens[*tokenCount].lexeme, "EOF");
-    (*tokenCount)++;
+    Token eof = {TOKEN_EOF, "EOF"};
+    tokens[(*tokenCount)++] = eof;
 
     fclose(file);
     return tokens;
